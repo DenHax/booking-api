7858dca14af92c6dc35cc3825df450cd3b2c4634
DenHax <whatxpw@gmail.com>
2025-11-19 00:12:09 +0300

    feat(api): add users, booking and events CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, EventsModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
