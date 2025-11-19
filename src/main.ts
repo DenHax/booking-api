b0e6a5518d5f354864d0405517c8873048a0a444
DenHax <whatxpw@gmail.com>
2025-11-19 17:15:14 +0300

    feat(api): add docs in swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookings service')
+    .setDescription(
+      "The booking API that provide booking rental services for clients' events",
+    )
+    .setVersion('1.0')
+    .addTag('bookings')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
