0ca27a114518bddf9657b2f629e11dbf959148ab
DenHax <whatxpw@gmail.com>
2025-11-19 17:17:35 +0300

    feat(users): update with prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from 'generated/prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        username: createUserDto.username,
+      },
+    });
+  }
+
+  findAll(): Promise<User[]> {
+    return this.prisma.user.findMany({
+      include: {
+        bookings: true,
+      },
+    });
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      include: {
+        bookings: true,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with id "${id}" not found.`);
+    }
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with id "${id}" not found.`);
+    }
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with id "${id}" not found.`);
+    }
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
